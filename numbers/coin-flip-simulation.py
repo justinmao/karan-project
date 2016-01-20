f790c2647d7270c86806fc6abc7958f3aa3cde4f
Justin Mao <justinmao@live.com>
2016-01-19 22:02:20 -0500

    moved from fork project

---
+++ numbers/coin-flip-simulation.py
@@ -0,0 +1,47 @@
+"""
+Coin Flip Simulation:
+Write some code that simulates flipping a single coin however many times the
+user decides. The code should record the outcomes and count the number of tails
+and heads.
+
+Concept(s): random numbers, global variables
+"""
+
+from random import randint
+
+heads = 0;
+tails = 0;
+
+def main():
+    while True:
+        run = raw_input("Flip a coin? (y/n/reset): ")
+        if run == "y":
+            print(flip())
+        elif run == "n":
+            pass
+        elif run == "reset":
+            global heads
+            global tails
+            heads = 0;
+            tails = 0;
+        else: 
+            print("Input invalid.")
+
+def flip():
+    value = ""
+    i = randint(0,1)
+    global heads
+    global tails
+    if i == 0:
+        heads += 1;
+        value = "heads"
+    else:
+        tails += 1;
+        value = "tails"
+    return ("The coin landed on "+value+", for a total of "+str(heads)+" heads "
+        "and "+str(tails)+" tails.")
+        
+def invalid_input():
+    return "Input must be a positive integer."
+    
+main()
---
+++ numbers/fibonnaci-sequence.py
@@ -0,0 +1,29 @@
+"""
+Fibonacci Sequence:
+Enter a number and have the program generate the Fibonacci sequence to that 
+number or to the Nth number.
+
+Concept(s): recursion, error handling
+"""
+
+def main():
+    while True:
+        try:
+            n = int(raw_input("Enter a number: "))
+            print(fibonacci(n))
+        except ValueError:
+            print(invalid_input())
+
+def fibonacci(n):
+    if isinstance(n, int) and n > 0:
+        if n == 1 or n == 2:
+            return 1
+        elif n >= 2:
+            return fibonacci(n-1) + fibonacci(n-2)
+    else:
+        return invalid_input()
+
+def invalid_input():
+    return "Input must be a positive integer."
+    
+main()
