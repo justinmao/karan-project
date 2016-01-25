7a55e7cad08bca28870d968126a3d574fadbddad
Justin Mao <justinmao@live.com>
2016-01-25 14:23:31 -0500

    completed reverse-a-string

---
+++ text/reverse-a-string.py
@@ -0,0 +1,13 @@
+"""
+Reverse a String:
+Enter a string and the program will reverse it and print it out.
+
+Concept(s): simplicity
+"""
+
+def main():
+    while True:
+        s = raw_input("Enter a string: ")
+        print(s[::-1])
+        
+main()
