2922534fb6d1f84c7729ec10ef446c900c65e04e
Justin Mao <justinmao@live.com>
2016-02-16 18:23:09 -0500

    completed prime factorization

--- numbers/prime-factorization.py
+++ numbers/prime-factorization.py
@@ -3,19 +3,20 @@ Prime Factorization:
 Have the user enter a number and find all Prime Factors (if there are any) and 
 display them.
 
-Concept(s): nested loop
+Concept(s): nested loops, prime numbers, exceptions
 """
 
 def main():
-    print("This program will return the prime factors of a number.")
+    print "This program will return the prime factors of a number."
     while True:
         try:
             n = int(raw_input("Enter a number: "))
-            find_primes(n)
+            print str(factorization(n))[1:-1]
+                
         except ValueError:
             invalid_input()
 
-def find_primes(n): # find primes up to than n
+def find_primes(n): # find primes up to n
     primes = []
     for i in range(2, n+1):
         divisible = False
@@ -24,9 +25,24 @@ def find_primes(n): # find primes up to than n
                 divisible = True
         if divisible == False:
             primes.append(i)
-    print(primes)
+    return primes
 
+def factorization(n):
+    if isinstance(n, int) and n > 0:
+        if n == 1: return [1]
+        primes = find_primes(n) # prime numbers up to n
+        factors = []
+        remainder = n
+        while remainder != 1:
+            for prime in primes:
+                if remainder%prime == 0:
+                    remainder = remainder/prime
+                    factors.append(prime)
+        factors.sort()
+        return factors
+    else:
+        raise ValueError
 def invalid_input():
-    print("Input must be a positive integer.")
+    print "Input must be a positive integer."
     
 main()
